62750b028330ca2d5b0e528ef22bd61f4ac4028e
pramodrajarapu <pramodrajarapu@gmail.com>
2023-08-28 23:27:48 +0530

    Services written for judy brand

---
+++ Brand/Brand/src/test/java/com/Brand/Brand/BrandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Brand.Brand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Brand/Brand/src/test/java/controller/FirstRestController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/services")
+public class FirstRestController {
+    @RequestMapping(value = "/getServiceName", method = RequestMethod.GET)
+    public @ResponseBody String getServiceName() {
+        return "Judy Brand";
+    }
+
+
+}
